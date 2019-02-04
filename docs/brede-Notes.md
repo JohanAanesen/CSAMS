90498917b588024f6118d6f1e94cf88918a19003
Brede F. Klausen <BredeFK@stud.ntnu.no>
2019-02-04 18:00:42 +0100

    #8 add 'brede-Notes.md' My notes through the work. add 'user.html' as page for user profile. update 'userhandler.go' to use new html file

---
+++ docs/brede-Notes.md
@@ -0,0 +1,16 @@
+# The Log of Bread
+
+## Week One
+### Monday - 04/02/19
+ *Started late today because of Super Bowl airing until 04.00 at the night.*
+* Finished setting up the environment.
+* Started working/preparing for task 8(User Profile).
+* Watched a tutorial about bootstrap.
+
+### Tuesday - 05/02/19
+
+### Wednesday - 06/02/19
+
+### Thursday - 07/02/19
+
+### Friday - 08/02/19
--- handlers/userhandler.go
+++ handlers/userhandler.go
@@ -1,13 +1,30 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func UserHandler(w http.ResponseWriter, r *http.Request){
 
+
 	//check that user is logged in
+		// code
+
+
+
 
 	//fetch users information from server
 
 	//parse information with template
+	w.WriteHeader(http.StatusOK)
+
+	temp, err := template.ParseFiles("web/user.html")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	temp.Execute(w, nil)
 
 }
