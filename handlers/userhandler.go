6819a3583d33a3fa3a87e9cd1ea2cd526ce96662
Brede F. Klausen <BredeFK@stud.ntnu.no>
2019-02-05 12:14:21 +0100

    #8 update 'userhandler.go' I updated the file to send data to the html file

--- handlers/userhandler.go
+++ handlers/userhandler.go
@@ -14,8 +14,38 @@ func UserHandler(w http.ResponseWriter, r *http.Request){
 
 
 
-
 	//fetch users information from server
+
+
+	// TODO remove this with actual information
+	type class struct {
+		Code string
+		Name string
+		Link string
+	}
+
+	type user struct {
+		Name string
+		PrimaryEmail string
+		SecondaryEmail string
+		Classes []class
+	}
+
+	mobile := class{"IMT1337", "Mobile Development", "#"}
+	app := class{"IMT4200", "Application Development", "#"}
+	cloud := class{"IMT8008", "Cloud Technologies", "/"}
+	www := class{"IMT0880", "WWW technology", "/"}
+
+	classes := []class{
+		mobile,
+		app,
+		cloud,
+		www,
+	}
+
+	data := user{"Ola Nordmann", "olano@stud.ntnu.no", "", classes}
+	// TODO : end here
+
 
 	//parse information with template
 	w.WriteHeader(http.StatusOK)
@@ -25,6 +55,6 @@ func UserHandler(w http.ResponseWriter, r *http.Request){
 		log.Fatal(err)
 	}
 
-	temp.Execute(w, nil)
+	temp.Execute(w, data)
 
 }
