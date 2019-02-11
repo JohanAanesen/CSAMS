213b47828e422f96ca5d250c76601aa9182ff38a
Brede Fritjof Klausen <bredefk@stud.ntnu.no>
2019-02-11 11:48:11 +0100

    #16 - add logdb.go - started on #16 log to db

--- db/logdb.go
+++ db/logdb.go
@@ -1,1 +1,38 @@
 package db
+
+import (
+	"log"
+	"time"
+)
+
+type category string
+
+const (
+	ChangeName     category = "CHANGE-NAME"
+	ChangeEmail    category = "CHANGE-EMAIL"
+	ChangePassword category = "CHANGE-PASSWORD"
+)
+
+// LogToDB adds logs to the database when an user/admin does something noteworthy
+func LogToDB(userID int, category category) bool {
+
+	timeStamp := time.Now().UnixNano()
+
+	// Convert back to date
+	// tm := time.Unix(0, timeStamp)
+
+	// Add values in sql query
+	rows, err := DB.Query("INSERT INTO logs(userid, timestamp, log) VALUES (?, ?, ?)", userID, timeStamp, category)
+
+	// Handle possible error
+	if err != nil {
+		log.Fatal(err.Error())
+		return false
+	}
+
+	// Close
+	defer rows.Close()
+
+	// Nothing went wrong -> return true
+	return true
+}
