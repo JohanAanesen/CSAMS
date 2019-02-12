c2b52f6f80889e414c9c6a74a914927025a38b31
bredefk <bredefk@stud.ntnu.no>
2019-02-12 11:05:02 +0100

    #16- update - updated the logs table

--- config/database.sql
+++ config/database.sql
@@ -16,7 +16,7 @@ USE cs53;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+01:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
@@ -76,9 +76,14 @@ CREATE TABLE `course`
 
 CREATE TABLE `logs`
 (
-  `userid`    int(11)                             NOT NULL,
-  `timestamp` bigint                              NOT NULL,
-  `log`       varchar(256) COLLATE utf8_danish_ci NOT NULL
+  `userid`           int(11)                            NOT NULL,
+  `timestamp`        datetime                           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `activity`         varchar(32) COLLATE utf8_danish_ci NOT NULL,
+  `assignmentID`     int(11)                                     DEFAULT NULL,
+  `courseID`         int(11)                                     DEFAULT NULL,
+  `userAssignmentID` int(11)                                     DEFAULT NULL,
+  `oldvalue`         varchar(64) COLLATE utf8_danish_ci          DEFAULT NULL,
+  `newValue`         varchar(64) COLLATE utf8_danish_ci          DEFAULT NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
   COLLATE = utf8_danish_ci;
@@ -196,6 +201,11 @@ INSERT INTO `usercourse` (`userid`, `courseid`)
 VALUES (3, 1),
        (3, 2),
        (4, 3);
+
+INSERT INTO `logs` (`userid`, `activity`, `assignmentID`, `courseID`, `userAssignmentID`, `oldvalue`,
+                    `newValue`)
+VALUES ('1', 'changedName', NULL, NULL, NULL, 'Ola Nordmann', 'Ola Svenskemann'),
+       ('1', 'changedName', NULL, NULL, NULL, 'Ola Nordmann', 'Ola Svenskemann');
 
 -- end --
 
