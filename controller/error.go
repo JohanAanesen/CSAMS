fd468c39df2e01fe2ca5ac13a012d45ffeeba3a2
Svein Are Danielsen <sveindani@gmail.com>
2019-02-13 15:35:55 +0100

    Refactor optimizing code

--- controller/error.go
+++ controller/error.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/shared/view"
 	"net/http"
-	"strings"
 )
 
 // ErrorHandler handles displaying errors for the clients
@@ -21,37 +20,10 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request, status int) {
 }
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusNotFound)
-
-	var admin string
-	if strings.HasPrefix(r.RequestURI, "/admin") {
-		admin = "admin/"
-	}
-
-	v := view.New(r)
-	v.Name = admin + "error"
-
-	v.Vars["ErrorCode"] = http.StatusNotFound
-	v.Vars["ErrorMessage"] = http.StatusText(http.StatusNotFound)
-
-	v.Render(w)
+	// TODO (Svein): Check if client is trying to access dashboard, or any other sites, and send to /error or something
+	ErrorHandler(w, r, http.StatusNotFound)
 }
 func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusMethodNotAllowed)
-
-	var admin string
-	// Check if we it is a
-	if strings.HasPrefix(r.RequestURI, "/admin") {
-		admin = "admin/"
-	}
-
-	v := view.New(r)
-	v.Name = admin + "error"
-
-	v.Vars["ErrorCode"] = http.StatusMethodNotAllowed
-	v.Vars["ErrorMessage"] = http.StatusText(http.StatusMethodNotAllowed)
-
-	v.Render(w)
+	// TODO (Svein): Check if client is trying to access dashboard, or any other sites, and send to /error or something
+	ErrorHandler(w, r, http.StatusMethodNotAllowed)
 }
