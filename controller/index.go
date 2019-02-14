de83e07d3b18c094d115ec8832d0e0857820158b
bredefk <bredefk@stud.ntnu.no>
2019-02-14 13:02:51 +0100

    #23 - update - Can now add user to course

--- controller/index.go
+++ controller/index.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/shared/db"
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/shared/session"
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/shared/view"
@@ -24,4 +25,37 @@ func IndexGET(w http.ResponseWriter, r *http.Request) {
 	v.Vars["Auth"] = user.Authenticated
 	v.Vars["Courses"] = db.GetCoursesToUser(user.ID)
 	v.Render(w)
+}
+
+// JoinCoursePOST adds user to course
+func JoinCoursePOST(w http.ResponseWriter, r *http.Request) {
+
+	// Check if course exists
+	course := db.CourseExists(r.FormValue("courseID"))
+
+	// If course ID == -1, it doesn't exist
+	if course.ID == -1 {
+		ErrorHandler(w, r, http.StatusNotFound)
+		return
+	}
+
+	// Get user
+	user := session.GetUserFromSession(r)
+
+	// Check that user isn't in this class
+	if db.UserExistsInCourse(user.ID, course.ID) {
+		ErrorHandler(w, r, http.StatusBadRequest)
+		return
+	}
+
+	// Add user to course if possible
+	if !db.AddUserToCourse(user.ID, course.ID) {
+		ErrorHandler(w, r, http.StatusBadRequest)
+		return
+	}
+
+	// TODO : Give feedback to user
+	fmt.Println("Added to: " + course.Code + " - " + course.Name)
+
+	IndexGET(w, r)
 }
--- shared/db/coursedb.go
+++ shared/db/coursedb.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/model"
+	"log"
 )
 
 //GetCoursesToUser returns all the courses to the user
@@ -43,4 +44,85 @@ func GetCoursesToUser(userID int) model.Courses {
 	}
 
 	return courses
+}
+
+func CourseExists(uniqueID string) model.Course {
+	rows, err := GetDB().Query("SELECT course.* FROM course WHERE id = ?", uniqueID)
+	if err != nil {
+		fmt.Println(err.Error()) // TODO : log error
+
+		// returns empty course if it fails
+		return model.Course{ID: -1}
+	}
+
+	for rows.Next() {
+		var id int
+		var courseCode string
+		var courseName string
+		var teacher int
+		var description string
+		var year string
+		var semester string
+
+		rows.Scan(&id, &courseCode, &courseName, &teacher, &description, &year, &semester)
+
+		// Fill course object/struct
+		return model.Course{
+			ID:          id,
+			Code:        courseCode,
+			Name:        courseName,
+			Teacher:     teacher,
+			Description: description,
+			Year:        year,
+			Semester:    semester,
+		}
+	}
+
+	return model.Course{ID: -1}
+}
+
+// UserExistsInCourse checks if user exists in course
+func UserExistsInCourse(userID int, courseID int) bool {
+
+	// Checks if user exists in course
+	rows, err := GetDB().Query("SELECT * FROM usercourse WHERE userid = ? AND courseid = ?", userID, courseID)
+
+	// return false if not
+	if err != nil {
+		log.Println(err.Error())
+
+		return false
+	}
+
+	for rows.Next() {
+		var userid int
+		var courseid int
+
+		rows.Scan(&userid, &courseid)
+
+		// return true if user exists in course
+		return true
+	}
+
+	// default: return false
+	return false
+}
+
+// AddUserToCourse adds the user to a course
+func AddUserToCourse(userID int, courseID int) bool {
+
+	// Sql query
+	rows, err := GetDB().Query("INSERT INTO `usercourse` (`userid`, `courseid`) VALUES (?, ?)", userID, courseID)
+
+	// Return false if error
+	if err != nil {
+		log.Fatal(err.Error())
+		return false
+	}
+
+	// Close this
+	defer rows.Close()
+
+	// return true
+	return true
 }
