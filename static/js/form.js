57609a466b44ee9b648101bd778efeee31a4c524
Svein Are Danielsen <sveindani@gmail.com>
2019-02-19 10:15:24 +0100

    12 - added dynamic choices for radio's

--- static/js/form.js
+++ static/js/form.js
@@ -10,20 +10,24 @@
 
     /**
      *
+     * @param {object} settings
      * @constructor
      */
-    let Form = function(selector) {
-        /**
-         *
-         * @type {HTMLElement}
-         */
-        this.output = document.getElementById(selector);
-
+    let Form = function(settings) {
         /**
          *
          * @type {Object[]}
          */
         this.fields = [];
+
+        /**
+         *
+         * @type {{output: HTMLElement, weighted: boolean}}
+         */
+        this.settings = {
+            output: document.querySelector(settings.selector),
+            weighted: settings.weighted || false,
+        };
 
         /**
          * Add a new field into the fields array
@@ -53,7 +57,7 @@
          * Creates a new field in the 'fields' array, then renders all.
          */
         this.newField = function() {
-            this.fields.push(new Field());
+            this.fields.push(new Field(this.settings));
             this.renderAll();
         };
 
@@ -61,10 +65,10 @@
          * Clean the output HTML, then render all fields in it.
          */
         this.renderAll = function() {
-            this.output.innerHTML = '';
+            this.settings.output.innerHTML = '';
 
             this.fields.forEach((e, i) => {
-                this.output.appendChild(e.render(i));
+                this.settings.output.appendChild(e.render(i));
             });
         };
 
@@ -85,7 +89,7 @@
      *
      * @constructor
      */
-    let Field = function() {
+    let Field = function(settings) {
         /**
          *
          * @type {string}
@@ -112,22 +116,42 @@
 
         /**
          *
+         * @type {number}
+         */
+        this.weight = 0;
+
+        /**
+         *
+         * @type {object}
+         */
+        this.settings = {
+            weighted: settings.weighted || false,
+        };
+
+        /**
+         *
          * @param {number} id
          * @returns {HTMLElement}
          */
         this.render = function(id) {
-            let card, cardHeader, cardBody, h2, button, collapse, formGroup, label, select, input, option, small;
+            // Declaring all variable names
+            let card, cardHeader, cardBody, h2, button, collapse,
+                formGroup, label, select, input, option, small, textarea;
 
+            // Defining the card-wrapper
             card = document.createElement('div');
             card.classList.add(...['card']);
 
+            // Defining the card-header
             cardHeader = document.createElement('div');
             cardHeader.classList.add(...['card-header']);
             cardHeader.id = `heading_${id}`;
 
+            // Defining the card-title
             h2 = document.createElement('h2');
             h2.classList.add(...['mb-0']);
 
+            // Defining the accordion button for the card
             button = document.createElement('button');
             button.classList.add(...['btn', 'btn-link']);
             button.setAttribute('type', 'button');
@@ -137,18 +161,22 @@
             button.setAttribute('aria-expanded', 'false');
             button.innerText = `Question ${id+1}`;
 
+            // Appends button into title, and title into card-header
             h2.appendChild(button);
             cardHeader.appendChild(h2);
 
+            // Defining the collapsing wrapper
             collapse = document.createElement('div');
             collapse.classList.add(...['collapse']);
             collapse.id = `collapse_${id}`;
             collapse.setAttribute('aria-labelledby', `header_${id}`);
-            collapse.setAttribute('data-parent', '#formAccordion'); // TODO (Svein): Fix this
+            collapse.setAttribute('data-parent', '#formAccordion'); // TODO (Svein): Fix this, make the selector an argument for the form !?!?
 
+            // Defining the card-body
             cardBody = document.createElement('div');
             cardBody.classList.add(...['card-body']);
 
+            // Appending the card-body into the collapsing wrapper
             collapse.appendChild(cardBody);
 
             // == INPUT TYPE START ==
@@ -182,12 +210,43 @@
 
             select.addEventListener('change', e => {
                 this.type = e.target.value;
+                // TODO (Svein): Show choices on change for 'radio'
+                if (this.type === TYPES.RADIO) {
+                    document.getElementById(`choices_form-group_${id}`).classList.remove(...['sr-only']);
+                } else {
+                    document.getElementById(`choices_form-group_${id}`).classList.add(...['sr-only']);
+                }
             });
 
             formGroup.appendChild(label);
             formGroup.appendChild(select);
             cardBody.appendChild(formGroup);
             // == INPUT TYPE END ==
+
+            // == CHOICES START ==
+            formGroup = document.createElement('div');
+            formGroup.classList.add(...['form-group', 'sr-only']);
+            formGroup.id = `choices_form-group_${id}`;
+
+            label = document.createElement('label');
+            label.setAttribute('for', `choices_${id}`);
+            label.innerText = 'Choices';
+
+            textarea = document.createElement('textarea');
+            textarea.classList.add(...['form-control']);
+            textarea.setAttribute('name', `choices_${id}`);
+            textarea.id = `choices_${id}`;
+
+            small = document.createElement('small');
+            small.classList.add(...['form-text', 'text-muted']);
+            small.innerText = 'Separate choices per line.';
+
+            formGroup.appendChild(label);
+            formGroup.appendChild(textarea);
+            formGroup.appendChild(small);
+            cardBody.appendChild(formGroup);
+            // == CHOICES END ==
+
 
             // == LABEL START ==
             formGroup = document.createElement('div');
@@ -270,6 +329,35 @@
             cardBody.appendChild(formGroup);
             // == ORDER END ==
 
+            // Check if form is weighted
+            if (settings.weighted) {
+                // == WEIGHT START ==
+                formGroup = document.createElement('div');
+                formGroup.classList.add(...['form-group']);
+
+                label = document.createElement('label');
+                label.setAttribute('for', `weight_${id}`);
+                label.innerText = 'Weight';
+
+                input = document.createElement('input');
+                input.classList.add(...['form-control']);
+                input.setAttribute('type', 'number');
+                input.setAttribute('min', '0');
+                input.setAttribute('name', `weight_${id}`);
+                input.id = `weight_${id}`;
+                input.value = this.weight;
+                // EventListener to the input-field, saving the value
+                input.addEventListener('keyup', e => {
+                    e.preventDefault();
+                    this.weight = parseInt(e.target.value);
+                });
+
+                formGroup.appendChild(label);
+                formGroup.appendChild(input);
+                cardBody.appendChild(formGroup);
+                // == WEIGHT END ==
+            }
+
             card.appendChild(cardHeader);
             card.appendChild(collapse);
 
@@ -286,13 +374,18 @@
                 name: this.name,
                 label: this.label,
                 order: this.order,
+                weight: this.weight,
             }
         };
     };
 
+    // TODO (Svein): This needs to be moved in the foot afterwards
     let button = document.getElementById('addField');
     let exportJSON = document.getElementById('export');
-    let form = new Form('formAccordion');
+    let form = new Form({
+        selector: '#formAccordion',
+        weighted: false,
+    });
 
     button.addEventListener('click', () => {
         form.newField();
