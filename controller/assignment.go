aa39d6facc9977f05c766c00bdbf03883ebfdd7a
Svein Are Danielsen <sveindani@gmail.com>
2019-02-27 09:26:28 +0100

    9 - Update assignment view

--- controller/assignment.go
+++ controller/assignment.go
@@ -2,8 +2,14 @@ package controller
 
 import (
 	"fmt"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/model"
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/shared/view"
+	"github.com/gorilla/mux"
+	"github.com/shurcooL/github_flavored_markdown"
+	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 //AssignmentGET serves assignment page to users
@@ -34,8 +40,32 @@ func AssignmentGET(w http.ResponseWriter, r *http.Request) {
 }
 
 func AssignmentSingleGET(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	assignmentID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Println(err)
+		ErrorHandler(w, r, http.StatusInternalServerError)
+	}
+
+	assignmentRepo := model.AssignmentRepository{}
+	assignment, err := assignmentRepo.GetSingle(assignmentID)
+	if err != nil {
+		log.Println(err)
+		ErrorHandler(w, r, http.StatusInternalServerError)
+	}
+
+	descriptionMD := []byte(assignment.Description)
+	description := github_flavored_markdown.Markdown(descriptionMD)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
 	v := view.New(r)
 	v.Name = "assignment"
+
+	v.Vars["Assignment"] = assignment
+	v.Vars["Description"] = template.HTML(description)
 
 	v.Render(w)
 }
