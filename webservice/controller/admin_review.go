cd4732768fd71cee2e86b1bf8dc7399bb289bd44
Svein Are Danielsen <sveindani@gmail.com>
2019-03-02 18:57:05 +0100

    Added db functions for review

--- webservice/controller/admin_review.go
+++ webservice/controller/admin_review.go
@@ -1,10 +1,31 @@
 package controller
 
-import "net/http"
+import (
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/model"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/shared/view"
+	"log"
+	"net/http"
+)
 
 // AdminReviewGET handles GET-requests @ /admin/review
 func AdminReviewGET(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 
+	var reviewRepo = model.ReviewRepository{}
+
+	submissions, err := reviewRepo.GetAll()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	v := view.New(r)
+	v.Name = "admin/submission/index"
+
+	v.Vars["Submissions"] = submissions
+
+	v.Render(w)
 }
 
 // AdminReviewCreateGET handles GET-requests @ /admin/review/create
--- webservice/model/review.go
+++ webservice/model/review.go
@@ -6,7 +6,7 @@ import "github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignmen
 type Review struct {
 	ID     int   `json:"id" db:"id"`
 	FormID int   `json:"-" db:"form_id"`
-	Form   *Form `json:"form"`
+	Form   Form `json:"form"`
 }
 
 type ReviewRepository struct {}
@@ -38,6 +38,98 @@ func (repo *ReviewRepository) Insert(form Form) error {
 		query := "INSERT INTO fields (form_id, type, name, label, description, priority, weight, choices) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
 		// Execute the query
 		rows, err := db.GetDB().Query(query, int(formID), field.Type, field.Name, field.Label, field.Description, field.Order, field.Weight, field.Choices)
+		// Check for error
+		if err != nil {
+			return err
+		}
+		// Close the connection
+		rows.Close()
+	}
+
+	// Return no error
+	return nil
+}
+
+// GetAll returns all submission in the database
+func (repo *ReviewRepository) GetAll() ([]Review, error) {
+	// Declare return slice
+	var result []Review
+	// Create query-string
+	query := "SELECT id, form_id FROM review"
+	// Perform query
+	rows, err := db.GetDB().Query(query)
+	// Check for error
+	if err != nil {
+		return result, err
+	}
+	// Close connection
+	defer rows.Close()
+
+	// Loop through rows
+	for rows.Next() {
+		// Declare a single Submission
+		var review = Review{}
+		// Scan the data from the rows
+		err = rows.Scan(&review.ID, &review.FormID)
+		// Check for error
+		if err != nil {
+			return result, err
+		}
+
+		// Append scan-result to result
+		result = append(result, review)
+	}
+
+	// Declare a FormRepository
+	var formRepo = FormRepository{}
+	// Loop through all submissions
+	for index, review := range result {
+		// Get form from database
+		form, err := formRepo.Get(review.FormID)
+		// Check for error
+		if err != nil {
+			return result, nil
+		}
+		// Get the form
+		review.Form = form
+		// Set the new values
+		result[index] = review
+	}
+
+	return result, nil
+}
+
+func (repo *ReviewRepository) Update(form Form) error {
+	query := "UPDATE forms SET prefix=?, name=?, description=? WHERE id=?"
+	tx, err := db.GetDB().Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = db.GetDB().Exec(query, form.Prefix, form.Name, form.Description, form.ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	query = "DELETE FROM fields WHERE form_id=?"
+	rows, err := db.GetDB().Query(query, form.ID)
+	if err != nil {
+		return err
+	}
+	rows.Close()
+
+	// Loop trough fields in the forms
+	for _, field := range form.Fields {
+		// Insertion query
+		query := "INSERT INTO fields (form_id, type, name, label, description, priority, weight, choices) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
+		// Execute the query
+		rows, err := db.GetDB().Query(query, form.ID, field.Type, field.Name, field.Label, field.Description, field.Order, field.Weight, field.Choices)
 		// Check for error
 		if err != nil {
 			return err
