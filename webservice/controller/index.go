353de72b73b4b41cd23424d939061ee204b5c472
Johan Aanesen <johanaan@stud.ntnu.no>
2019-03-03 15:41:37 +0100

    #Alpha add can now see active assignments on the frontpage

--- webservice/controller/index.go
+++ webservice/controller/index.go
@@ -6,6 +6,7 @@ import (
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/shared/view"
 	"log"
 	"net/http"
+	"time"
 )
 
 // TODO : maybe remove/refactor global variable later :/
@@ -23,15 +24,40 @@ func IndexGET(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
-	//course repo
+	// repo's
 	courseRepo := &model.CourseRepository{}
+	assignmentRepo := model.AssignmentRepository{}
 
 	//get courses to user
-	courses, err := courseRepo.GetAllToUserSorted(session.GetUserFromSession(r).ID)
+	courses, err := courseRepo.GetAllToUserSorted(user.ID)
 	if err != nil {
 		log.Println(err)
 		ErrorHandler(w, r, http.StatusInternalServerError)
 		return
+	}
+
+	//need custom struct to get the coursecode
+	type ActiveAssignment struct {
+		Assignment model.Assignment
+		CourseCode string
+	}
+
+	var activeAssignments []ActiveAssignment
+
+	for _, course := range courses{ //iterate all courses
+		assignments ,err := assignmentRepo.GetAllFromCourse(course.ID) //get assignments from course
+		if err != nil {
+			log.Println(err)
+			ErrorHandler(w, r, http.StatusInternalServerError)
+			return
+		}
+
+		for _, assignment := range assignments { //go through all it's assignments again
+			if time.Now().After(assignment.Publish) && time.Now().Before(assignment.Deadline) { //save all 'active' assignments
+				activeAssignments = append(activeAssignments, ActiveAssignment{Assignment:assignment, CourseCode:course.Code})
+			}
+		}
+
 	}
 
 	// Set values
@@ -39,6 +65,7 @@ func IndexGET(w http.ResponseWriter, r *http.Request) {
 	v.Name = "index"
 
 	v.Vars["Courses"] = courses
+	v.Vars["Assignments"] = activeAssignments
 	v.Vars["Message"] = joinedCourse
 
 	v.Render(w)
