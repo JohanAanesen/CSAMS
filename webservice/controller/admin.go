6f6ebd121960f75c485b37c02a4021d43ef8531c
Johan Aanesen <johanaan@stud.ntnu.no>
2019-03-03 16:40:21 +0100

    #Alpha update fixed course_code to show on assignments on /admin aswell

--- webservice/controller/admin.go
+++ webservice/controller/admin.go
@@ -14,29 +14,45 @@ func AdminGET(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
+	// repo's
+	courseRepo := &model.CourseRepository{}
+	assignmentRepo := model.AssignmentRepository{}
+
+	//get courses to user/teacher
+	courses, err := courseRepo.GetAllToUserSorted(session.GetUserFromSession(r).ID)
+	if err != nil {
+		log.Println(err)
+		ErrorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+
+	//need custom struct to get the coursecode
+	type ActiveAssignment struct {
+		Assignment model.Assignment
+		CourseCode string
+	}
+
+	var activeAssignments []ActiveAssignment
+
+	for _, course := range courses{ //iterate all courses
+		assignments, err := assignmentRepo.GetAllFromCourse(course.ID) //get assignments from course
+		if err != nil {
+			log.Println(err)
+			ErrorHandler(w, r, http.StatusInternalServerError)
+			return
+		}
+
+		for _, assignment := range assignments { //go through all it's assignments again
+			activeAssignments = append(activeAssignments, ActiveAssignment{Assignment:assignment, CourseCode:course.Code})
+		}
+
+	}
+
 	v := view.New(r)
 	v.Name = "admin/index"
 
-	assignmentRepo := model.AssignmentRepository{}
-	assignments, err := assignmentRepo.GetAllToUserSorted(session.GetUserFromSession(r).ID)
-	if err != nil {
-		ErrorHandler(w, r, http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-
-	//course repo
-	courseRepo := &model.CourseRepository{}
-
-	courses, err := courseRepo.GetAllToUserSorted(session.GetUserFromSession(r).ID)
-	if err != nil {
-		ErrorHandler(w, r, http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-
 	v.Vars["Courses"] = courses
-	v.Vars["Assignments"] = assignments
+	v.Vars["Assignments"] = activeAssignments
 
 	v.Render(w)
 }
