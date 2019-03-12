f27716af3298ca9301f3196005f03aa3c0f693c7
Svein Are Danielsen <sveindani@gmail.com>
2019-03-12 14:37:45 +0100

    Added tests for all plugins

--- webservice/shared/view/plugin/splitchoices_test.go
+++ webservice/shared/view/plugin/splitchoices_test.go
@@ -6,19 +6,14 @@ import (
 	"html/template"
 	"log"
 	"testing"
-	"time"
 )
 
-func TestPrettyTime(t *testing.T) {
-	const parse = `{{PRETTYTIME .}}`
-	const expected = "15:04 01/02/2019"
+func TestSplitChoices(t *testing.T) {
+	const parse = `{{SPLIT_CHOICES .}}`
+	const input = "a,b,c"
+	const expected = "[a b c]"
 
-	input, err := time.Parse(time.RFC3339, "2019-01-02T15:04:05Z")
-	if err != nil {
-		log.Fatalf("time parsing: %s", err)
-	}
-
-	tmpl, err := template.New("deadlineDueTest").Funcs(plugin.PrettyTime()).Parse(parse)
+	tmpl, err := template.New("splitChoicesTest").Funcs(plugin.SplitChoices()).Parse(parse)
 	if err != nil {
 		log.Fatalf("parsing: %s", err)
 	}
