370adabd60cbb76558aad5057f935493f3caf8d4
JohanAanesen <johanaan@stud.ntnu.no>
2019-03-13 20:39:58 +0100

    #Alpha scheduler frontend panel update

--- webservice/controller/admin_scheduler.go
+++ webservice/controller/admin_scheduler.go
@@ -1,33 +1,59 @@
 package controller
 
 import (
+	"encoding/json"
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/shared/view"
-	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
-func AdminSchedulerGET(w http.ResponseWriter, r *http.Request){
+func AdminSchedulerGET(w http.ResponseWriter, r *http.Request) {
+	type PeerLoad struct {
+		Authentication string `json:"authentication"`
+		AssignmentID   int    `json:"assignment_id"`
+		SubmissionID   int    `json:"submission_id"`
+		Reviewers      int    `json:"reviewers"`
+	}
+
+	type PayLoad struct {
+		ID             int       `json:"id"`
+		Authentication string    `json:"authentication"`
+		ScheduledTime  time.Time `json:"scheduled_time"`
+		Task           string    `json:"task"`
+		AssignmentID   int       `json:"assignment_id"`
+		SubmissionID   int       `json:"submission_id"`
+		Data           PeerLoad  `json:"data"`
+	}
+
+	var payloads []PayLoad
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
 	v := view.New(r)
 	v.Name = "admin/scheduler/index"
 
-
-	resp, err := http.Get("http://"+os.Getenv("SCHEDULE_SERVICE"))
+	resp, err := http.Get("http://" + os.Getenv("SCHEDULE_SERVICE"))
 	if err != nil {
 		panic(err)
 	}
 
 	defer resp.Body.Close()
 
-	html, err := ioutil.ReadAll(resp.Body)
+	err = json.NewDecoder(resp.Body).Decode(&payloads)
 	if err != nil {
 		panic(err)
 	}
 
-	v.Vars["Content"] = string(html)
+	/*
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	*/
+
+	v.Vars["Payloads"] = payloads
 
 	v.Render(w)
 }
