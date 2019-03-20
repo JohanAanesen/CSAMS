2b61335a28d2fbf001f2744d2dc5f79efa2fbc93
Svein Are Danielsen <sveindani@gmail.com>
2019-03-20 23:16:39 +0100

    Added repository & service for user

---
+++ webservice/repositroy/user.go
@@ -0,0 +1,232 @@
+package repositroy
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/model"
+)
+
+// UserRepository struct
+type UserRepository struct {
+	db *sql.DB
+}
+
+// NewUserRepository func
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+// Fetch func
+func (repo *UserRepository) Fetch(id int) (*model.User, error) {
+	result := model.User{}
+	result.Authenticated = false
+
+	query := "SELECT id, name, email_student, teacher, email_private FROM users WHERE id = ?"
+
+	rows, err := repo.db.Query(query, id)
+	if err != nil {
+		return &result, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&result.ID, &result.Name, &result.EmailStudent,
+			&result.Teacher, &result.EmailPrivate)
+		if err != nil {
+			return &result, err
+		}
+	}
+
+	result.Authenticated = true
+	return &result, err
+}
+
+// FetchHash func
+func (repo *UserRepository) FetchHash(id int) (string, error) {
+	var result string
+
+	query := "SELECT password FROM users WHERE id = ?"
+
+	rows, err := repo.db.Query(query, id)
+	if err != nil {
+		return result, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&result)
+		if err != nil {
+			return result, err
+		}
+	}
+
+	return result, err
+}
+
+// FetchAll func
+func (repo *UserRepository) FetchAll() ([]*model.User, error) {
+	result := make([]*model.User, 0)
+
+	query := "SELECT id, name, email_student, teacher, email_private FROM users"
+
+	rows, err := repo.db.Query(query)
+	if err != nil {
+		return result, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		temp := model.User{}
+
+		err = rows.Scan(&temp.ID, &temp.Name, &temp.EmailStudent,
+			&temp.Teacher, &temp.EmailPrivate)
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, &temp)
+	}
+
+	return result, err
+}
+
+// FetchAllFromCourse func
+func (repo *UserRepository) FetchAllFromCourse(courseID int) ([]*model.User, error) {
+	result := make([]*model.User, 0)
+
+	query := "SELECT u.id, u.name, u.email_student, u.teacher, u.email_private FROM users AS u INNER JOIN usercourse AS uc ON u.id = uc.userid WHERE uc.courseid = ?"
+
+	rows, err := repo.db.Query(query, courseID)
+	if err != nil {
+		return result, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		temp := model.User{}
+
+		err = rows.Scan(&temp.ID, &temp.Name, &temp.EmailStudent,
+			&temp.Teacher, &temp.EmailPrivate)
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, &temp)
+	}
+
+	return result, err
+}
+
+// Insert func
+func (repo *UserRepository) Insert(user model.User, password string) (int, error) {
+	var id int64
+
+	query := "INSERT INTO users (name, email_student, teacher, password) VALUES (?, ?, 0, ?)"
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return int(id), err
+	}
+
+	rows, err := tx.Exec(query, user.Name, user.EmailStudent, password)
+	if err != nil {
+		tx.Rollback()
+		return int(id), err
+	}
+
+	id, err = rows.LastInsertId()
+	if err != nil {
+		tx.Rollback()
+		return int(id), err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return int(id), err
+	}
+
+	return int(id), err
+}
+
+// Update func
+func (repo *UserRepository) Update(id int, user model.User) error {
+	if id != user.ID {
+		return errors.New("update user, id mismatch")
+	}
+
+	query := "UPDATE users SET name = ?, email_private = ? WHERE id = ?"
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(query, user.Name, user.EmailPrivate, user.ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return err
+}
+
+// UpdatePassword func
+func (repo *UserRepository) UpdatePassword(id int, hashedPassword string) error {
+	query := "UPDATE users SET password = ? WHERE id = ?"
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(query, hashedPassword, id)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return err
+}
+
+// Delete func
+func (repo *UserRepository) Delete(id int) error {
+	query := "DELETE FROM users WHERE id = ?"
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(query, id)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return err
+}
---
+++ webservice/service/user.go
@@ -0,0 +1,105 @@
+package service
+
+import (
+	"database/sql"
+	"errors"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/model"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/repositroy"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/shared/util"
+)
+
+// UserService struct
+type UserService struct {
+	userRepo *repositroy.UserRepository
+}
+
+// NewUserService func
+func NewUserService(db *sql.DB) *UserService {
+	return &UserService{
+		userRepo: repositroy.NewUserRepository(db),
+	}
+}
+
+// Fetch func
+func (s *UserService) Fetch(id int) (*model.User, error) {
+	return s.userRepo.Fetch(id)
+}
+
+// FetchHash func
+func (s *UserService) FetchHash(id int) (string, error) {
+	return s.userRepo.FetchHash(id)
+}
+
+// FetchAll func
+func (s *UserService) FetchAll() ([]*model.User, error) {
+	return s.userRepo.FetchAll()
+}
+
+// FetchAllFromCourse func
+func (s *UserService) FetchAllFromCourse(courseID int) ([]*model.User, error) {
+	return s.userRepo.FetchAllFromCourse(courseID)
+}
+
+// Register func
+func (s *UserService) Register(user model.User, password string) (int, error) {
+	hashed, err := util.GenerateFromPassword(password)
+	if err != nil {
+		return 0, err
+	}
+
+	return s.userRepo.Insert(user, hashed)
+}
+
+// Authenticate func
+func (s *UserService) Authenticate(email, password string) (model.User, error) {
+	// Create empty user
+	result := model.User{}
+	// Get all users
+	users, err := s.userRepo.FetchAll()
+	if err != nil {
+		return result, err
+	}
+	// Set found to false
+	found := false
+	// Loop through all users
+	for _, u := range users {
+		// Check if any if it's emails match
+		if u.EmailStudent == email || u.EmailPrivate == email {
+			// Set user, and found to true
+			result = *u
+			found = true
+			break
+		}
+	}
+	// Not found
+	if !found {
+		return result, errors.New("user not found by given email")
+	}
+	// Get hash for user
+	hash, err := s.FetchHash(result.ID)
+	if err != nil {
+		return result, err
+	}
+	// Compare hash and password
+	err = util.CompareHashAndPassword(password, hash)
+	if err != nil {
+		return result, err
+	}
+
+	return result, err
+}
+
+// Update func
+func (s *UserService) Update(id int, user model.User) error {
+	return s.userRepo.Update(id, user)
+}
+
+// UpdatePassword func
+func (s *UserService) UpdatePassword(id int, password string) error {
+	hash, err := util.GenerateFromPassword(password)
+	if err != nil {
+		return err
+	}
+
+	return s.userRepo.UpdatePassword(id, hash)
+}
