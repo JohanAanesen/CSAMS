33633ad0051fe9a1948270e61fbbceb90045c9cd
Svein Are Danielsen <sveindani@gmail.com>
2019-03-21 12:50:27 +0100

    Added more members to the struct

--- webservice/model/submission_answer.go
+++ webservice/model/submission_answer.go
@@ -1,15 +1,22 @@
 package model
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 // SubmissionAnswer struct
 type SubmissionAnswer struct {
-	ID           int       `json:"id"`
-	UserID       int       `json:"user_id"`
-	AssignmentID int       `json:"assignment_id"`
-	SubmissionID int       `json:"submission_id"`
-	Type         string    `json:"type"`
-	Answer       string    `json:"answer"`
-	Comment      string    `json:"comment"`
-	Submitted    time.Time `json:"submitted"`
+	ID           int            `json:"id"`
+	UserID       int            `json:"user_id"`
+	AssignmentID int            `json:"assignment_id"`
+	SubmissionID int            `json:"submission_id"`
+	Type         string         `json:"type"`
+	Name         string         `json:"name"`
+	Label        string         `json:"label"`
+	Description  string         `json:"description"`
+	HasComment   bool           `json:"has_comment"`
+	Answer       string         `json:"answer"`
+	Comment      sql.NullString `json:"comment"`
+	Submitted    time.Time      `json:"submitted"`
 }
