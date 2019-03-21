05d5562b40e6e79866e70c063631a92b163e6562
Svein Are Danielsen <sveindani@gmail.com>
2019-03-21 12:52:21 +0100

    Added service for submission answers

---
+++ webservice/repositroy/submission_answer.go
@@ -0,0 +1,181 @@
+package repositroy
+
+import (
+	"database/sql"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/model"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/shared/util"
+)
+
+// SubmissionAnswerRepository struct
+type SubmissionAnswerRepository struct {
+	db *sql.DB
+}
+
+// NewSubmissionAnswerRepository func
+func NewSubmissionAnswerRepository(db *sql.DB) *SubmissionAnswerRepository {
+	return &SubmissionAnswerRepository{
+		db: db,
+	}
+}
+
+// Fetch func
+func (repo *SubmissionAnswerRepository) Fetch(id int) (*model.SubmissionAnswer, error) {
+	result := model.SubmissionAnswer{}
+
+	query := "SELECT id, user_id, assignment_id, submission_id, type, name, label, answer, comment, submitted FROM user_submissions WHERE id = ?"
+
+	rows, err := repo.db.Query(query, id)
+	if err != nil {
+		return &result, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&result.ID, &result.UserID, &result.AssignmentID, &result.SubmissionID,
+			&result.Type, &result.Name, &result.Label, &result.Answer, &result.Comment, &result.Submitted)
+		if err != nil {
+			return &result, err
+		}
+	}
+
+	return &result, err
+}
+
+// FetchAll func
+func (repo *SubmissionAnswerRepository) FetchAll() ([]*model.SubmissionAnswer, error) {
+	result := make([]*model.SubmissionAnswer, 0)
+
+	query := "SELECT id, user_id, assignment_id, submission_id, type, name, label, answer, comment, submitted FROM user_submissions"
+
+	rows, err := repo.db.Query(query)
+	if err != nil {
+		return result, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		temp := model.SubmissionAnswer{}
+
+		err = rows.Scan(&temp.ID, &temp.UserID, &temp.AssignmentID, &temp.SubmissionID,
+			&temp.Type, &temp.Name, &temp.Label, &temp.Answer, &temp.Comment, &temp.Submitted)
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, &temp)
+	}
+
+	return result, err
+}
+
+// FetchAllForUserAndAssignment func
+func (repo *SubmissionAnswerRepository) FetchAllForUserAndAssignment(userID, assignmentID int) ([]*model.SubmissionAnswer, error) {
+	result := make([]*model.SubmissionAnswer, 0)
+
+	query := "SELECT id, user_id, assignment_id, submission_id, type, name, label, answer, comment, submitted FROM user_submissions WHERE user_id = ? AND assignment_id = ?"
+
+	rows, err := repo.db.Query(query, userID, assignmentID)
+	if err != nil {
+		return result, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		temp := model.SubmissionAnswer{}
+
+		err = rows.Scan(&temp.ID, &temp.UserID, &temp.AssignmentID, &temp.SubmissionID,
+			&temp.Type, &temp.Name, &temp.Label, &temp.Answer, &temp.Comment, &temp.Submitted)
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, &temp)
+	}
+
+	return result, err
+}
+
+// Insert func
+func (repo *SubmissionAnswerRepository) Insert(answer model.SubmissionAnswer) (int, error) {
+	var id int64
+
+	query := "INSERT INTO user_submissions (user_id, assignment_id, submission_id, type, name, label, answer, comment, submitted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return int(id), err
+	}
+
+	submitted := util.ConvertTimeStampToString(util.GetTimeInCorrectTimeZone())
+	rows, err := tx.Exec(query, answer.UserID, answer.AssignmentID,
+		answer.SubmissionID, answer.Type, answer.Name, answer.Label, answer.Answer, answer.Comment, submitted)
+	if err != nil {
+		tx.Rollback()
+		return int(id), err
+	}
+
+	id, err = rows.LastInsertId()
+	if err != nil {
+		tx.Rollback()
+		return int(id), err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return int(id), err
+	}
+
+	return int(id), err
+}
+
+// CountForAssignment func
+func (repo *SubmissionAnswerRepository) CountForAssignment(assignmentID int) (int, error) {
+	var count int
+
+	query := "SELECT COUNT(DISTINCT user_id) FROM user_submissions WHERE assignment_id = ?"
+
+	rows, err := repo.db.Query(query, assignmentID)
+	if err != nil {
+		return count, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&count)
+		if err != nil {
+			return count, err
+		}
+	}
+
+	return count, err
+}
+
+// Update func
+func (repo *SubmissionAnswerRepository) Update(answer model.SubmissionAnswer) error {
+	query := "UPDATE user_submissions SET answer = ?, comment = ?, submitted = ? WHERE user_id = ? AND assignment_id = ?"
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	submitted := util.ConvertTimeStampToString(util.GetTimeInCorrectTimeZone())
+	_, err = tx.Exec(query, answer.Answer, answer.Comment, submitted, answer.UserID, answer.AssignmentID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return err
+}
---
+++ webservice/service/submission_answer.go
@@ -0,0 +1,79 @@
+package service
+
+import (
+	"database/sql"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/model"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/repositroy"
+)
+
+// SubmissionAnswerService struct
+type SubmissionAnswerService struct {
+	submissionAnswerRepo *repositroy.SubmissionAnswerRepository
+}
+
+// NewSubmissionAnswerService func
+func NewSubmissionAnswerService(db *sql.DB) *SubmissionAnswerService {
+	return &SubmissionAnswerService{
+		submissionAnswerRepo: repositroy.NewSubmissionAnswerRepository(db),
+	}
+}
+
+// Fetch func
+func (s *SubmissionAnswerService) Fetch(id int) (*model.SubmissionAnswer, error) {
+	return s.submissionAnswerRepo.Fetch(id)
+}
+
+// FetchAll func
+func (s *SubmissionAnswerService) FetchAll() ([]*model.SubmissionAnswer, error) {
+	return s.submissionAnswerRepo.FetchAll()
+}
+
+// CountForAssignment func
+func (s *SubmissionAnswerService) CountForAssignment(assignmentID int) (int, error) {
+	return s.submissionAnswerRepo.CountForAssignment(assignmentID)
+}
+
+// HasUserSubmitted func
+func (s *SubmissionAnswerService) HasUserSubmitted(assignmentID, userID int) (bool, error) {
+	answers, err := s.submissionAnswerRepo.FetchAll()
+	if err != nil {
+		return false, err
+	}
+
+	for _, item := range answers {
+		if item.AssignmentID == assignmentID && item.UserID == userID {
+			return true, err
+		}
+	}
+
+	return false, err
+}
+
+// FetchUserAnswers func
+func (s *SubmissionAnswerService) FetchUserAnswers(userID, assignmentID int) ([]*model.SubmissionAnswer, error) {
+	return s.submissionAnswerRepo.FetchAllForUserAndAssignment(userID, assignmentID)
+}
+
+// Upload func
+func (s *SubmissionAnswerService) Upload(answers []*model.SubmissionAnswer) error {
+	for _, item := range answers {
+		_, err := s.submissionAnswerRepo.Insert(*item)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Update func
+func (s *SubmissionAnswerService) Update(answers []*model.SubmissionAnswer) error {
+	for _, item := range answers {
+		err := s.submissionAnswerRepo.Update(*item)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
