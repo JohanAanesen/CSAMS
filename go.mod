a31f58d34cf83c5fc03fcf82291f7b9ed48ebce1
bredefk <bredefk@stud.ntnu.no>
2019-04-01 16:37:09 +0200

    beta-brede - new - SendMail works already, have to switch computer to refactor code

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-.\gomodule github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments
+module github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments
 
 go 1.12
 
--- mailservice/handler.go
+++ mailservice/handler.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"net/smtp"
 )
 
 // HandlerGET handles GET requests
 func HandlerGET(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "This API only accepts POST-requests", http.StatusBadRequest)
 	return
+	// SendMail([]string{"RECEIVERMAIL"}, "MESSAGE", "SUBJECT")
 }
 
 // HandlerPOST handles POST requests
 func HandlerPOST(w http.ResponseWriter, r *http.Request) {
+	//SendEmail(w, r)
+}
 
+// SendMail sends mail to people
+func SendMail(to []string, message string, subject string) {
 
+	auth := smtp.PlainAuth("", "SENDEREMAIL", "PASSWORD", "smtp.gmail.com")
+
+	msg := []byte("To: " + to[0] + "\nSubject: " + subject + "\n" + message)
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, "SENDEREMAIL", to, msg)
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
