7865218f4ca2c44f18e57e94b5488047af032f96
bredefk <bredefk@stud.ntnu.no>
2019-04-02 13:57:37 +0200

    beta-brede - new - added mailservice to webservice

---
+++ webservice/shared/mail/mailservice.go
@@ -0,0 +1,42 @@
+package mail
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// SendMail sends mail to the user with mailservice
+func (mail Mail) SendMail(email string) error {
+
+	// Make a temporary struct for posting to mailservice
+	jsonData := struct {
+		Email string `json:"email"`
+		Link  string `json:"link"`
+	}{
+		Email: email,
+		Link:  "https://www.google.no/",
+	}
+
+	// This is just sending the request
+	jsonValue, err := json.Marshal(jsonData)
+	if err != nil {
+		return err
+	}
+
+	url := "http://localhost:8085" // mailservice
+
+	if os.Getenv("MAIL_SERVICE") != "" {
+		url = "http://" + os.Getenv("MAIL_SERVICE") // mailservice address changed in env var
+	}
+
+	_, err = http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+		return err
+	}
+
+	return nil
+}
