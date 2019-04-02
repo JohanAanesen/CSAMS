e6af2ad1a6daa952eddc707082a7b8c2ee0189d0
bredefk <bredefk@stud.ntnu.no>
2019-04-02 15:23:33 +0200

    beta-brede - new - added insert and match functions for the new table

--- webservice/repositroy/forgotten_password.go
+++ webservice/repositroy/forgotten_password.go
@@ -1,6 +1,9 @@
 package repositroy
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/model"
+)
 
 // ForgottenPassRepository struct
 type ForgottenPassRepository struct {
@@ -12,4 +15,59 @@ func NewForgottenPassRepository(db *sql.DB) *ForgottenPassRepository {
 	return &ForgottenPassRepository{
 		db: db,
 	}
+}
+
+// Insert inserts a new forgottenpass in the db
+func (repo *ForgottenPassRepository) Insert(forgottenPass model.ForgottenPass) (int, error) {
+	var id int
+
+	query := "INSERT INTO `forgotten_password` (`hash`, `user_id`, `timestamp`) VALUES (?, ?, ?)"
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return id, err
+	}
+
+	_, err = tx.Exec(query, forgottenPass.Hash, forgottenPass.UserID, forgottenPass.TimeStamp)
+	if err != nil {
+		tx.Rollback()
+		return id, err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return id, err
+	}
+
+	return id, nil
+}
+
+// Match checks if the hash exists in the db
+func (repo *ForgottenPassRepository) Match(hash string) (bool, error) {
+
+	query := "SELECT COUNT(id) FROM `forgotten_password` WHERE `hash` = ?"
+
+	rows, err := repo.db.Query(query, hash)
+	if err != nil {
+		return false, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var count int
+
+		err = rows.Scan(&count)
+		if err != nil {
+			return false, err
+		}
+
+		// If count is over 0, the hash exists in the db
+		if count > 0 {
+			return true, nil
+		}
+	}
+
+	return false, err
 }
--- webservice/service/forgotten_password.go
+++ webservice/service/forgotten_password.go
@@ -2,17 +2,44 @@ package service
 
 import (
 	"database/sql"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/model"
 	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/repositroy"
+	"github.com/JohanAanesen/NTNU-Bachelor-Management-System-For-CS-Assignments/webservice/shared/util"
 )
 
 // ForgottenPassService struct
 type ForgottenPassService struct {
-	assignmentRepo *repositroy.ForgottenPassRepository
+	forgottenPassRepo *repositroy.ForgottenPassRepository
 }
 
 // NewForgottenPassService func
 func NewForgottenPassService(db *sql.DB) *ForgottenPassService {
 	return &ForgottenPassService{
-		assignmentRepo: repositroy.NewForgottenPassRepository(db),
+		forgottenPassRepo: repositroy.NewForgottenPassRepository(db),
 	}
+}
+
+// Insert func
+func (s *ForgottenPassService) Insert(forgottenPass model.ForgottenPass) (int, error) {
+	var err error
+
+	// First hash the hash for more security
+	forgottenPass.Hash, err = util.GenerateFromPassword(forgottenPass.Hash)
+	if err != nil {
+		return -1, err
+	}
+
+	return s.forgottenPassRepo.Insert(forgottenPass)
+}
+
+// Match first hashes the hash and then checks if the hash match in the db
+func (s *ForgottenPassService) Match(hash string) (bool, error) {
+
+	//  First hash the hash for more security
+	hashed, err := util.GenerateFromPassword(hash)
+	if err != nil {
+		return false, err
+	}
+
+	return s.forgottenPassRepo.Match(hashed)
 }
