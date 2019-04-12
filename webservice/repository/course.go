1997350855d9fd9d21b849c8a372d0668dc97823
Svein Are Danielsen <sveindani@gmail.com>
2019-04-12 14:10:32 +0200

    Added comments to file

--- webservice/repository/course.go
+++ webservice/repository/course.go
@@ -5,112 +5,119 @@ import (
 	"github.com/JohanAanesen/CSAMS/webservice/model"
 )
 
-// CourseRepository struct
+// CourseRepository represents the layer between the database and the Course-model
 type CourseRepository struct {
 	db *sql.DB
 }
 
-// NewCourseRepository func
+// NewCourseRepository returns a pointer to a new CourseRepository
 func NewCourseRepository(db *sql.DB) *CourseRepository {
 	return &CourseRepository{
 		db: db,
 	}
 }
 
-// Fetch func
+// Fetch a single course from the database
 func (repo *CourseRepository) Fetch(id int) (*model.Course, error) {
+	// Initialize an empty course
 	result := model.Course{}
-
+	// Query string
 	query := "SELECT id, hash, coursecode, coursename, teacher, description, year, semester FROM course WHERE id = ?"
-
+	// Perform query-string with id as a parameter
 	rows, err := repo.db.Query(query, id)
 	if err != nil {
-		return &result, err
+		return nil, err
 	}
-
+	// Close connection
 	defer rows.Close()
-
+	// Loop through rows
 	for rows.Next() {
+		// Scan all columns from row
 		err = rows.Scan(&result.ID, &result.Hash, &result.Code,
 			&result.Name, &result.Teacher, &result.Description,
 			&result.Year, &result.Semester)
 		if err != nil {
-			return &result, err
+			return nil, err
 		}
 	}
-
-	return &result, err
+	// Return result
+	return &result, nil
 }
 
-// FetchAll func
+// FetchAll courses from the database
 func (repo *CourseRepository) FetchAll() ([]*model.Course, error) {
+	// Create empty course slice
 	result := make([]*model.Course, 0)
-
+	// Query string
 	query := "SELECT id, hash, coursecode, coursename, teacher, description, year, semester FROM course"
-
+	// Perform query
 	rows, err := repo.db.Query(query)
 	if err != nil {
-		return result, err
+		return nil, err
 	}
-
+	// Close connection
 	defer rows.Close()
-
+	// Loop through rows
 	for rows.Next() {
+		// Temporary course object
 		temp := model.Course{}
-
+		// Scan columns from row
 		err = rows.Scan(&temp.ID, &temp.Hash, &temp.Code,
 			&temp.Name, &temp.Teacher, &temp.Description,
 			&temp.Year, &temp.Semester)
 		if err != nil {
-			return result, err
+			return nil, err
 		}
-
+		// Append temporary course to result
 		result = append(result, &temp)
 	}
-
-	return result, err
+	// Return result
+	return result, nil
 }
 
-// FetchAllForUserOrdered func
+// FetchAllForUserOrdered all courses for a user, ordered
 func (repo *CourseRepository) FetchAllForUserOrdered(userID int) ([]*model.Course, error) {
+	// Create empty course slice
 	result := make([]*model.Course, 0)
-
+	// Query string
 	query := "SELECT c.id, c.hash, c.coursecode, c.coursename, c.teacher, c.description, c.year, c.semester FROM course AS c INNER JOIN usercourse AS uc ON c.id = uc.courseid WHERE uc.userid = ? ORDER BY c.year DESC, c.semester ASC, c.coursename ASC"
-
+	// Perform query
 	rows, err := repo.db.Query(query, userID)
 	if err != nil {
-		return result, err
+		return nil, err
 	}
-
+	// Close connection
 	defer rows.Close()
-
+	// Loop through rows
 	for rows.Next() {
+		// Create temporary course object
 		temp := model.Course{}
-
+		// Scan columns from the row
 		err = rows.Scan(&temp.ID, &temp.Hash, &temp.Code,
 			&temp.Name, &temp.Teacher, &temp.Description,
 			&temp.Year, &temp.Semester)
 		if err != nil {
-			return result, err
+			return nil, err
 		}
-
+		// Append temporary object to result
 		result = append(result, &temp)
 	}
-
-	return result, err
+	// Return result
+	return result, nil
 }
 
-// Insert func
+// Insert course to the database
 func (repo *CourseRepository) Insert(course model.Course) (int, error) {
+	// Integer to hold the id of last inserted row
 	var id int64
-
+	// Query string
 	query := "INSERT INTO course (hash, coursecode, coursename, teacher, description, year, semester) VALUES (?, ?, ?, ?, ?, ?, ?)"
-
+	// Begin transaction
 	tx, err := repo.db.Begin()
 	if err != nil {
 		return int(id), err
 	}
-
+	// Execute query with parameters
 	rows, err := tx.Exec(query, course.Hash, course.Code,
 		course.Name, course.Teacher, course.Description,
 		course.Year, course.Semester)
@@ -118,61 +125,63 @@ func (repo *CourseRepository) Insert(course model.Course) (int, error) {
 		tx.Rollback()
 		return int(id), err
 	}
-
+	// Commit transaction
 	err = tx.Commit()
 	if err != nil {
 		tx.Rollback()
 		return int(id), err
 	}
-
+	// Get last inserted ID
 	id, err = rows.LastInsertId()
 	if err != nil {
 		tx.Rollback()
 		return int(id), err
 	}
-
-	return int(id), err
+	// Return result
+	return int(id), nil
 }
 
-// InsertUser func
+// InsertUser to a course, gives a relationship between a user and a course in the database
 func (repo *CourseRepository) InsertUser(userID int, courseID int) error {
-
+	// Query string
 	query := "INSERT INTO usercourse (userid, courseid) VALUES (?, ?)"
-
+	// Begin transaction
 	tx, err := repo.db.Begin()
 	if err != nil {
 		return err
 	}
-
+	// Execute query with parameters
 	_, err = tx.Exec(query, userID, courseID)
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
+	// Commit transaction
 	err = tx.Commit()
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
-	return err
+	// Return no error
+	return nil
 }
 
-// UserInCourse func
+// UserInCourse checks if user is in given course
 func (repo *CourseRepository) UserInCourse(userID, courseID int) (bool, error) {
+	// Query string
 	query := "SELECT COUNT(DISTINCT userid) FROM usercourse WHERE userid =? AND courseid = ?"
-
+	// Perform query with parameters
 	rows, err := repo.db.Query(query, userID, courseID)
 	if err != nil {
 		return false, err
 	}
-
+	// Close connection
 	defer rows.Close()
-
+	// Check if it returned any rows
 	if rows.Next() {
+		// Integer for storing the result
 		var result int
-
+		// Scan column from the row
 		err = rows.Scan(&result)
 		if err != nil {
 			return false, err
@@ -184,106 +193,112 @@ func (repo *CourseRepository) UserInCourse(userID, courseID int) (bool, error) {
 		}
 	}
 
-	return false, err
+	// Return false
+	return false, nil
 }
 
-// Update func
+// Update course in the database
 func (repo *CourseRepository) Update(id int, course model.Course) error {
+	// Query string
 	query := "UPDATE course SET coursecode = ?, coursename = ?, description = ?, semester = ? WHERE id = ?"
-
+	// Begin transaction
 	tx, err := repo.db.Begin()
 	if err != nil {
 		return err
 	}
-
+	// Execute query with parameters
 	_, err = tx.Exec(query, course.Code, course.Name, course.Description, course.Semester, id)
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
+	// Commit transaction
 	err = tx.Commit()
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
-	return err
+	// All good, return no error
+	return nil
 }
 
-// Delete func
+// Delete course from the database, based on course ID
 func (repo *CourseRepository) Delete(id int) error {
+	// Query string
 	query := "DELETE FROM course WHERE id = ?"
-
+	// Begin transaction
 	tx, err := repo.db.Begin()
 	if err != nil {
 		return err
 	}
-
+	// Execute query with parameter
 	_, err = tx.Exec(query, id)
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
+	// Commit transaction
 	err = tx.Commit()
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
-	return err
+	// All good, no error
+	return nil
 }
 
-// RemoveUser func
+// RemoveUser removes the relationship between a user and a course in the database
 func (repo *CourseRepository) RemoveUser(userID, courseID int) error {
+	// Query string
 	query := "DELETE FROM usercourse WHERE userid = ? AND courseid = ?"
-
+	// Begin transaction
 	tx, err := repo.db.Begin()
 	if err != nil {
 		return err
 	}
-
+	// Execute query with parameters
 	_, err = tx.Exec(query, userID, courseID)
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
+	// Commit transaction
 	err = tx.Commit()
 	if err != nil {
 		tx.Rollback()
 		return err
 	}
-
-	return err
+	// All good, no error
+	return nil
 }
 
-// FetchAllForUser func
+// FetchAllForUser fetches all courses for a given user
 func (repo *CourseRepository) FetchAllForUser(userID int) ([]*model.Course, error) {
+	// Empty course slice
 	result := make([]*model.Course, 0)
-
+	// Query string
 	query := "SELECT c.id, c.hash, c.coursecode, c.coursename, c.teacher, c.description, c.year, c.semester FROM course AS c INNER JOIN usercourse AS uc ON c.id = uc.courseid WHERE uc.userid = ? ORDER BY c.year DESC, c.semester ASC, c.coursename ASC"
-
+	// Perform query
 	rows, err := repo.db.Query(query, userID)
 	if err != nil {
-		return result, err
+		return nil, err
 	}
-
+	// Close connection
 	defer rows.Close()
-
+	// Loop through rows
 	for rows.Next() {
+		// Temporary course object
 		temp := model.Course{}
-
+		// Scan columns from row
 		err = rows.Scan(&temp.ID, &temp.Hash, &temp.Code,
 			&temp.Name, &temp.Teacher, &temp.Description,
 			&temp.Year, &temp.Semester)
 		if err != nil {
-			return result, err
+			return nil, err
 		}
-
+		// Append temporary object to result
 		result = append(result, &temp)
 	}
-
-	return result, err
+	// Return result
+	return result, nil
 }
