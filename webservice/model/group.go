cf3bd2871a8b4beda3d6ba257f0f55760edcb395
Svein Are Danielsen <sveindani@gmail.com>
2019-04-15 13:56:17 +0200

    Added group model

---
+++ webservice/model/group.go
@@ -0,0 +1,9 @@
+package model
+
+// Group struct
+type Group struct {
+	ID           int    `json:"id"`
+	AssignmentID int    `json:"assignment_id"`
+	Name         string `json:"name"`
+	Users        []User `json:"users"`
+}
