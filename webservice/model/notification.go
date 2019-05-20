43e962be22c1fcd2de3513d532ae18dd35b8b026
Johan Aanesen <johanaan@stud.ntnu.no>
2019-05-20 13:59:34 +0200

    added notification model

--- webservice/model/notification.go
+++ webservice/model/notification.go
@@ -1,1 +1,9 @@
 package model
+
+// Notification hold the data for a single notification
+type Notification struct {
+	ID      int    `json:"id" db:"id"`
+	UserID  int    `json:"user_id" db:"user_id"`
+	URL     string `json:"url" db:"url"`
+	Message string `json:"message" db:"message"`
+}
